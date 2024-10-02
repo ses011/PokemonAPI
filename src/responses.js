9942b84fe12748c9c4db72072c7c0a7a5c0a5fa2
Sarah Schneider (RIT Student) <ses1431@rit.edu>
2024-10-02 14:16:05 -0400

    Base file setup

---
+++ src/responses.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+const style = fs.readFileSync(`${__dirname}/../client/style.css`);
+
+
+module.exports = {
+
+
+}
