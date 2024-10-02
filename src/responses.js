1a0063c889f25005c8a9eb8417a2cb5ca92daf09
Sarah Schneider (RIT Student) <ses1431@rit.edu>
2024-10-02 14:46:15 -0400

    Added data json file, started html input fields

--- src/responses.js
+++ src/responses.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 const style = fs.readFileSync(`${__dirname}/../client/style.css`);
-
+const data = fs.readFileSync(`./pokedex.json`);
 
 module.exports = {
 
--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const http = require('http');
+
 const responses = require('./responses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
 
 const onRequest = (request, response) => {
 	console.log(request.url);
