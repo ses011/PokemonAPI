9ab57ff250f7bc8785f6665caac8daf669a65af1
Sarah Schneider (RIT Student) <ses1431@rit.edu>
2024-10-04 14:40:40 -0400

    responses to requests for client-side html, js, and style files. Some errors that I'm hoping are machine-related

--- src/responses.js
+++ src/responses.js
@@ -2,9 +2,46 @@ const fs = require('fs');
 
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 const style = fs.readFileSync(`${__dirname}/../client/style.css`);
+const cCode = fs.readFileSync(`${__dirname}/../client/client.js`);
 const data = fs.readFileSync(`./pokedex.json`);
 
+const sendResponseClient = (content, response, type) => {
+    response.writeHead(200, { "Content-Type": type });
+    response.write(content);
+    response.end();
+}
+
+const sendResponseData = (code, content, response) => {
+
+
+}
+
+const byName = () => {
+
+
+}
+
+const byType = () => {
+
+
+}
+
+const getIndex = (req, res) => {
+    sendResponseClient(index, res, 'text/html');
+}
+
+const getStyle = (req, res) => {
+    sendResponseClient(style, res, 'text/css');
+}
+
+const getCode = (req, res) => {
+    sendResponseClient(cCode, res, 'text/javascript');
+}
+
 module.exports = {
-
-
+    getIndex,
+    getStyle,
+    getCode,
+    byName,
+    byType
 }
--- src/server.js
+++ src/server.js
@@ -4,10 +4,26 @@ const responses = require('./responses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
+const urlStruct = {
+	'/': responses.getIndex,
+	'': responses.getIndex,
+	index: responses.getIndex,
+	'/style.css': responses.getStyle,
+	'/client.js': responses.getCode,
+};  
 
 const onRequest = (request, response) => {
 	console.log(request.url);
 
+	const protocol = request.connection.encrypted ? 'https' : 'http';
+  	const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
+	request.query = Object.fromEntries(parsedUrl.searchParams);
+
+	if (urlStruct[parsedUrl.pathname]) {
+		urlStruct[parsedUrl.pathname](request, response);
+	  } else {
+		responses.notFound(request, response);
+	  }
 }
 
 http.createServer(onRequest).listen(port, () => {
