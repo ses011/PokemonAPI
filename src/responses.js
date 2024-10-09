60fff60d752ebe668b2c6631a612bf9f3dbe91a0
Sarah Schneider (RIT Student) <ses1431@rit.edu>
2024-10-09 14:40:19 -0400

    Fix eslint errors. Small changes to strong against data and code

--- src/responses.js
+++ src/responses.js
@@ -6,114 +6,111 @@ const cCode = fs.readFileSync(`${__dirname}/../client/client.js`);
 const data = JSON.parse(fs.readFileSync(`${__dirname}/../src/pokedex.json`));
 const strong = JSON.parse(fs.readFileSync(`${__dirname}/../src/strengths.json`));
 
-const sendResponseData = (code, content, response, type) => {
-    if (type === "application/json") {
-        content = JSON.stringify(content);
-    }
+const sendResponseData = (code, val, response, type) => {
+  let content = val;
+  if (type === 'application/json') {
+    content = JSON.stringify(val);
+  }
 
-    response.writeHead(code, { 
-        "Content-Type": type,
-        "Content-Length": Buffer.byteLength(content, "utf8")
-    });
+  response.writeHead(code, {
+    'Content-Type': type,
+    'Content-Length': Buffer.byteLength(content, 'utf8'),
+  });
 
-    response.write(content);
-    response.end();
-}
+  response.write(content);
+  response.end();
+};
 
 const sendHead = (code, res) => {
-    res.writeHead(code);
-    res.end();
-}
+  res.writeHead(code);
+  res.end();
+};
 
 const getData = (req, res) => {
-    console.log("getData");
-    let n, t, e;
-    let selections = data;
+  let n; let t; let e;
+  let selections = data;
 
-    if (req.query.name) {
-        n = req.query.name.toLowerCase();
-        console.log(`Name: ${n}`);
-        selections = selections.filter((pokemon) => pokemon.name.toLowerCase().includes(n));
-    }
-    if (req.query.type) {
-        t = req.query.type;
-        selections = selections.filter((pokemon) => pokemon.type.includes(t));
-    }
-    if (req.query.effective) {
-        e = req.query.effective;
-        console.log(e);
-        
-        selections = selections.filter((pokemon) => {
-            for (const elem of strong.e) {
-                if (pokemon.type.includes(elem)) {
-                    console.log(pokemon.type);
-                    return true;
-                }
-            }
-            return false;
-        });
+  if (req.query.name) {
+    n = req.query.name.toLowerCase();
+    selections = selections.filter((pokemon) => pokemon.name.toLowerCase().includes(n));
+  }
+  if (req.query.type) {
+    t = req.query.type;
+    selections = selections.filter((pokemon) => pokemon.type.includes(t));
+  }
+  if (req.query.effective) {
+    e = req.query.effective;
+    console.log(e);
+    console.log(strong);
 
-    }
+    selections = selections.filter((pokemon) => {
+      for (const elem of strong.e) {
+        if (pokemon.type.includes(elem)) {
+          return true;
+        }
+      }
+      return false;
+    });
+  }
 
+  if (selections.length === 0) {
+    sendHead(204, res);
+    return;
+  }
 
-    if (selections.length === 0) {
-        sendHead(204, res);
-        return;
-    }
-
-    sendResponseData(200, selections, res, "application/json");
-}   
+  sendResponseData(200, selections, res, 'application/json');
+};
 
 // Assumes req.body is a json object similar to how a single pokemon object is stored
 // Will likely change this later to not update at all if a field is left empty
 const editData = (req, res) => {
-    let poke = data[req.body.id - 1];
+  const poke = data[req.body.id - 1];
 
-    poke.name = req.body.name;
-    poke.img = req.body.name;
-    poke.type = req.body.type;
-    poke.height = req.body.height;
-    poke.weight = req.body.weight;
-    poke.weaknesses = req.body.weaknesses;
+  poke.name = req.body.name;
+  poke.img = req.body.name;
+  poke.type = req.body.type;
+  poke.height = req.body.height;
+  poke.weight = req.body.weight;
+  poke.weaknesses = req.body.weaknesses;
 
-    data[req.body.id - 1] = poke;
-    sendResponseData(202, "Pokemon updated", res, "text/plain");
-}
+  data[req.body.id - 1] = poke;
+  sendResponseData(202, 'Pokemon updated', res, 'text/plain');
+};
 
 // This causes data to be out of order, but should still work the same
 const addData = (req, res) => {
-    let newPoke = req.body;
+  const newPoke = req.body;
 
-    newPoke.id = data[data.length - 1].id + 1;
-    newPoke.num = newPoke.id.toString();
-    
-    data.push(newPoke);
+  newPoke.id = data[data.length - 1].id + 1;
+  newPoke.num = newPoke.id.toString();
 
-    sendResponseData(201, "Pokemon added", res, "text/plain");
-}
+  data.push(newPoke);
+
+  sendResponseData(201, 'Pokemon added', res, 'text/plain');
+};
 
 const getIndex = (req, res) => {
-    sendResponseData(200, index, res, 'text/html');
-}
+  sendResponseData(200, index, res, 'text/html');
+};
 
 const getStyle = (req, res) => {
-    sendResponseData(200, style, res, 'text/css');
-}
+  sendResponseData(200, style, res, 'text/css');
+};
 
 const getCode = (req, res) => {
-    sendResponseData(200, cCode, res, 'application/javascript');
-}
+  sendResponseData(200, cCode, res, 'application/javascript');
+};
 
 const notFound = (req, res) => {
-    sendHead(400, res);
-}
+  sendHead(400, res);
+};
 
 module.exports = {
-    getIndex,
-    getStyle,
-    getCode,
-    getData,
-    addData,
-    editData,
-    notFound
-}
+  getIndex,
+  getStyle,
+  getCode,
+  getData,
+  addData,
+  editData,
+  notFound,
+};
--- src/server.js
+++ src/server.js
@@ -5,61 +5,60 @@ const responses = require('./responses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
-	'/': responses.getIndex,
-	'': responses.getIndex,
-	index: responses.getIndex,
-	'/style.css': responses.getStyle,
-	'/client.js': responses.getCode,
-	'/search': responses.getData
-};  
+  '/': responses.getIndex,
+  '': responses.getIndex,
+  index: responses.getIndex,
+  '/style.css': responses.getStyle,
+  '/client.js': responses.getCode,
+  '/search': responses.getData,
+};
 
 const parseBody = (req, res, handler) => {
-	const body = [];
+  const body = [];
 
-	req.on('error', err => {
-		console.dir(err);
-		res.statusCode = 400;
-		res.end();
-	});
+  req.on('error', (err) => {
+    console.dir(err);
+    res.statusCode = 400;
+    res.end();
+  });
 
-	req.on("data", chunk => {
-		body.push(chunk);
-	})
+  req.on('data', (chunk) => {
+    body.push(chunk);
+  });
 
-	req.on("end", () => {
-		const bodyStr = Buffer.concat(body).toString();
-		req.body = query.parse(bodyStr);
-
-		handler(req, res);
-	})
-}
+  req.on('end', () => {
+    const bodyStr = Buffer.concat(body).toString();
+    req.body = query.parse(bodyStr);
+    // query ^ ???
+    handler(req, res);
+  });
+};
 
 const handlePost = (req, res, url) => {
-	if (url.pathname === "/addPokemon") {
-		parseBody(req, res, responses.addData);
-	}
-	else if (url.pathname === "/editPokemon") {
-		parseBody(req, res, responses.editData);
-	}
-}
+  if (url.pathname === '/addPokemon') {
+    parseBody(req, res, responses.addData);
+  } else if (url.pathname === '/editPokemon') {
+    parseBody(req, res, responses.editData);
+  }
+};
 
 const onRequest = (request, response) => {
-	console.log(request.url);
+  console.log(request.url);
 
-	const protocol = request.connection.encrypted ? 'https' : 'http';
-  	const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
-	request.query = Object.fromEntries(parsedUrl.searchParams);
+  const protocol = request.connection.encrypted ? 'https' : 'http';
+  const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
+  request.query = Object.fromEntries(parsedUrl.searchParams);
 
-	if (request.method === "POST") {
-		handlePost(request, response, parsedUrl);
-	}
-	if (urlStruct[parsedUrl.pathname]) {
-		urlStruct[parsedUrl.pathname](request, response);
-	} else {
-	responses.notFound(request, response);
-	}
-}
+  if (request.method === 'POST') {
+    handlePost(request, response, parsedUrl);
+  }
+  if (urlStruct[parsedUrl.pathname]) {
+    urlStruct[parsedUrl.pathname](request, response);
+  } else {
+    responses.notFound(request, response);
+  }
+};
 
 http.createServer(onRequest).listen(port, () => {
-	console.log(`Listening on port 127.0.0.1:${port}`);
+  console.log(`Listening on port 127.0.0.1:${port}`);
 });
